a52714fa9f460e6f5d475101b24f86bddabb129b
Tyler Smith <tjismith13@icloud.com>
2020-06-01 14:40:27 -0400

    GUI form

---
+++ src/Gameplay.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Gameplay {
+    private JLabel Title;
+    private JLabel TimeClock;
+    private JTextField AnswerField;
+    private JLabel QuestionField;
+    private JLabel HighScore;
+    private JLabel QuestionsCorrect;
+    private JButton StartButton;
+    private JLabel ActionMessage;
+
+    public Gameplay() {
+
+        StartButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        AnswerField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+            }
+        });
+    }
+}
