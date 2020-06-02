03829085efa0a67790c26fb170bec0d546e36f03
Tyler Smith <tjismith13@icloud.com>
2020-06-01 22:57:53 -0400

    Create Random and base of easy question generator

--- src/QuestionGenerator.java
+++ src/QuestionGenerator.java
@@ -1,3 +1,50 @@
+import java.util.Random;
+
 public class QuestionGenerator {
-    
+    Random random = new Random();
+
+
+    public String generateQuestion(boolean easy, boolean medium, boolean hard) {
+        if (easy) {
+            int firstNum = random.nextInt(10);
+            int secondNum = random.nextInt(10);
+
+            int signGenerator = random.nextInt(4);
+            String sign = " ";
+            if (signGenerator == 0) {
+                sign = "+";
+            }
+            if (signGenerator == 1) {
+                sign = "*";
+            }
+            if (signGenerator == 2) {
+                sign = "-";
+            }
+            if (signGenerator == 3) {
+                sign = "/";
+            }
+            return firstNum + sign + secondNum;
+        }
+        else if (medium) {
+            int firstNum = random.nextInt(13);
+            int secondNum = random.nextInt(10);
+
+            int signGenerator = random.nextInt(4);
+            String sign = " ";
+            if (signGenerator == 0) {
+                sign = "+";
+            }
+            if (signGenerator == 1) {
+                sign = "*";
+            }
+            if (signGenerator == 2) {
+                sign = "-";
+            }
+            if (signGenerator == 3) {
+                sign = "/";
+            }
+            return firstNum + sign + secondNum;
+        }
+
+    }
 }
