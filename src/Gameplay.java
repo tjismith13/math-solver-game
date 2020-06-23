055c6bf54b952ff83cdffe67ffdce018857bc377
Tyler Smith <tjismith13@icloud.com>
2020-06-23 16:28:00 -0400

    Working clock running thread, add comments

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -23,24 +23,50 @@ public class Gameplay {
     private String currentQ;
     private int score = 0;
     private Clock clock = new Clock();
-    private String timeRemaining;
     private IntChecker intChecker = new IntChecker();
     private int answer;
     private HighScore highScoreChecker = new HighScore();
 
-    public Gameplay() {
+    //Thread to be called when start/restart is pressed, creates and updates new countdown clock
+    private Thread clockThread = new Thread() {
+        public void run() {
+            clock.init(60);
+            while (clock.timeRemaining() >= 0) {
+                timeClock.setText(String.valueOf(clock.timeRemaining()));
+            }
+            if (clock.timeRemaining() < 1) {
+                //Game reset
+                timeClock.setText("0");
+                answerField.setText("Answer");
+                questionField.setText("Question");
+                actualAnswer.setText("");
 
+                //Update and display high score
+                highScoreChecker.updateHighScore(score);
+                highScore.setText("High Score: " + highScoreChecker.getHighScore());
+            }
+        }
+    };
+
+    public Gameplay() {
 
         //When start/restart is pressed
         startButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                clock.init(10);
+
+                //Start clock
+                clockThread.start();
+
+                //Set or Reset Game
                 answerField.setText("");
                 score = 0;
                 questionsCorrect.setText("Score " + score);
+
+                //Set question generator
                 currentQ = generator.generateQuestion(true, false);
                 questionField.setText(currentQ);
+
                 startButton.setText("Restart");
             }
         });
@@ -49,16 +75,14 @@ public class Gameplay {
         answerButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+                //Ensures game is started
                 if (startButton.getText().equals("Start")) answerField.setText("Press 'start' to begin!");
 
-                else if (clock.timeRemaining() <= 0) {
-                    timeClock.setText("0");
-                    answerField.setText("Answer");
-                    questionField.setText("Question");
-                    actualAnswer.setText("");
-                    highScoreChecker.updateHighScore(score);
-                    highScore.setText("High Score" + highScoreChecker.getHighScore());
-                } else {
+                //While game is happening, first 15 seconds
+                else if (clock.timeRemaining() >= 45){
+
+                    //Check that answer is an integer
                     String answerText = answerField.getText();
                     if (!intChecker.isInt(answerText)) answer = -1000;
 
@@ -66,25 +90,77 @@ public class Gameplay {
 
                     else answer = Integer.parseInt(answerText);
 
+                    //Check answer correctness and update score based on result
                     if (generator.correct(currentQ) == answer) {
                         score += 1;
                         actualAnswer.setText("Actual Answer:");
-                    } else actualAnswer.setText("Actual Answer: " + generator.correct(currentQ));
+                    }
 
+                    //If answer is wrong display correct answer
+                    else actualAnswer.setText("Actual Answer: " + generator.correct(currentQ));
+
+                    //Questions in easy mode
+                    questionsCorrect.setText("Score: " + score);
+                    currentQ = generator.generateQuestion(true, false);
+                    questionField.setText(currentQ);
+                    answerField.setText("");
+                }
+
+                //While game is happening, middle 30 seconds
+                else if (clock.timeRemaining() >= 15 && clock.timeRemaining() < 45){
+
+                    //Check that answer is an integer
+                    String answerText = answerField.getText();
+                    if (!intChecker.isInt(answerText)) answer = -1000;
+
+                    else if (answerText.isEmpty()) answer = -1000;
+
+                    else answer = Integer.parseInt(answerText);
+
+                    //Check answer correctness and update score based on result
+                    if (generator.correct(currentQ) == answer) {
+                        score += 1;
+                        actualAnswer.setText("Actual Answer:");
+                    }
+
+                    //If answer is wrong display correct answer
+                    else actualAnswer.setText("Actual Answer: " + generator.correct(currentQ));
+
+                    //Questions in medium difficulty mode
                     questionsCorrect.setText("Score: " + score);
                     currentQ = generator.generateQuestion(false, true);
                     questionField.setText(currentQ);
                     answerField.setText("");
-                    timeRemaining = String.valueOf(clock.timeRemaining());
-                    timeClock.setText(timeRemaining);
+                }
+                //While game is happening, last 15 seconds
+                else {
 
+                    //Check that answer is an integer
+                    String answerText = answerField.getText();
+                    if (!intChecker.isInt(answerText)) answer = -1000;
+
+                    else if (answerText.isEmpty()) answer = -1000;
+
+                    else answer = Integer.parseInt(answerText);
+
+                    //Check answer correctness and update score based on result
+                    if (generator.correct(currentQ) == answer) {
+                        score += 1;
+                        actualAnswer.setText("Actual Answer:");
+                    }
+
+                    //If answer is wrong display correct answer
+                    else actualAnswer.setText("Actual Answer: " + generator.correct(currentQ));
+
+                    //Questions in hard mode
+                    questionsCorrect.setText("Score: " + score);
+                    currentQ = generator.generateQuestion(false, false);
+                    questionField.setText(currentQ);
+                    answerField.setText("");
                 }
             }
         });
     }
-        public void updateClock() {
-            timeClock.setText(timeRemaining);
-        }
 
         public static void main(String[] args) {
             JFrame frame = new JFrame("frmMainScreen");
@@ -93,20 +169,5 @@ public class Gameplay {
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.pack();
             frame.setVisible(true);
-            Thread clockThread = new Thread() {
-                Clock thClock = new Clock();
-                public void run() {
-                    try {
-                        thClock.init(60);
-                        for(;;) {
-                            gameplay.timeClock.setText(String.valueOf(thClock.timeRemaining()));
-                        }
-                    }
-                    catch(Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            };
-            clockThread.start();
         }
     }
